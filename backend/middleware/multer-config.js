1c1102b302fe81443afca5d8f5badd3d26fb48bc
Alix Bocquier <alixbocqui@gmail.com>
2024-12-30 09:23:26 +0100

    :backend: ajout d'un livre fonctionnel

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -5,14 +5,25 @@ const MIME_TYPES = {
   "image/jpeg": "jpg",
   "image/png": "png",
 };
-
+/*
+const storage = multer.memoryStorage({
+  destination: (req, file, callback) => {
+    callback(null, "images");
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(" ").join("_");
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + "." + extension);
+  }
+});
+*/
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, "images");
   },
   filename: (req, file, callback) => {
     const name = file.originalname.split(" ").join("_");
-    const extension = MIME.TYPES[file.mimetype];
+    const extension = MIME_TYPES[file.mimetype];
     callback(null, name + Date.now() + "." + extension);
   }
 });
